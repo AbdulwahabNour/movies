3c0d90dd3758ef9681bbad7da1a2fdf36b32a75b
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-08-20 21:02:58 +0300

    Update User model with new structs and functions

--- internal/model/users/users.go
+++ internal/model/users/users.go
@@ -4,39 +4,51 @@ import (
 	"errors"
 	"time"
 
-	"golang.org/x/crypto/bcrypt"
+	"github.com/AbdulwahabNour/movies/internal/model/token"
 )
 
 type User struct {
 	ID             int64     `json:"id"`
-	CreateAt       time.Time `json:"create_at"`
-	Name           string    `json:"name" validate:"required,max=100"`
+	CreateAt       time.Time `json:"create_at" `
+	Name           string    `json:"name" validate:"required,max=100" `
 	Email          string    `json:"email" validate:"required,email,max=100"`
-	Password       string    `json:"password" validate:"required,min=10,max=200" `
-	HashedPassword []byte    `json:"-"`
-	Actived        bool      `json:"activated"`
+	Password       string    `json:"password,omitempty" validate:"required,min=8,max=50" `
+	HashedPassword []byte    `json:"-"  validate:"required"`
+	Activated      *bool     `json:"activated,omitempty"`
 	Version        string    `json:"version"`
 }
+type SignUpInput struct {
+	Name            string `json:"name" validate:"required,max=100"`
+	Email           string `json:"email" validate:"required,email,max=100"`
+	Password        string `json:"password" validate:"required,min=8,max=50" `
+	ConfirmPassword string `json:"confirm_password" validate:"required,min=8,max=50" `
+}
+type SignIn struct {
+	Email    string `json:"email" validate:"required,email,max=100"`
+	Password string `json:"password" validate:"required,min=8,max=200" `
+}
 
-func (u *User) SetHashPassword() error {
-	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
+type UserWithToken struct {
+	User  *User            `json:"user"`
+	Token *token.TokenPair `json:"token"`
+}
+
+func (u *User) SanitizePassword() {
+	u.Password = ""
+}
+
+func (u *SignUpInput) Check() error {
+	if u.Password != u.ConfirmPassword {
+		return errors.New("passwords do not match")
 	}
-	u.HashedPassword = hash
+
 	return nil
 }
-func (u *User) ComparePasswords(text string) (bool, error) {
+func (u *SignUpInput) Map() User {
 
-	err := bcrypt.CompareHashAndPassword(u.HashedPassword, []byte(text))
-	if err != nil {
-		switch {
-		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
-			return false, nil
-		default:
-			return false, err
-		}
+	return User{
+		Name:     u.Name,
+		Email:    u.Email,
+		Password: u.Password,
 	}
-
-	return true, nil
 }
