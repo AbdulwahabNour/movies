3dc2da5217a14977b76fea285c21e4787dd0eaa3
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-08-20 21:29:51 +0300

    Update user routes with login and register handlers

--- internal/users/delivery/http/routes.go
+++ internal/users/delivery/http/routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 func MapUsersRoutes(r *gin.RouterGroup, app users.Handler) {
-
-	r.GET("/users", app.CreateUserHandler)
+	r.POST("/users/login", app.SigInHandler)
+	r.POST("/users/register", app.SignUpHandler)
 	r.GET("/users/:email", app.GetUserByEmailHandler)
 	r.PUT("/users/:id", app.UpdateuUserHandler)
 	r.DELETE("/users/:id", app.DeleteUserHandler)
