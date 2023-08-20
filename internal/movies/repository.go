ccfc2885234e06973c8e9ed79669e6103b70d254
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-08-20 21:09:31 +0300

    Rename ListMoviesHandler function to ListMovies in movie repository

--- internal/movies/repository.go
+++ internal/movies/repository.go
@@ -9,7 +9,7 @@ import (
 type Repository interface {
 	CreateMovie(ctx context.Context, movie *model.Movie) error
 	GetMovie(ctx context.Context, id int64) (*model.Movie, error)
-	ListMoviesHandler(ctx context.Context, query *model.MovieSearchQuery) ([]*model.Movie, error)
+	ListMovies(ctx context.Context, query *model.MovieSearchQuery) ([]*model.Movie, error)
 	UpdateMovie(ctx context.Context, movie *model.Movie) error
 	DeleteMovie(ctx context.Context, id int64) error
 }
