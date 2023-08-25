df685571797156ae54ac075d5f815ec166933c3d
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-08-26 00:19:18 +0300

    Structured Movie Routes: Consistency and Middleware Integration

--- internal/movies/delivery/http/routes.go
+++ internal/movies/delivery/http/routes.go
@@ -1,16 +1,19 @@
 package http
 
 import (
+	"github.com/AbdulwahabNour/movies/internal/middlewares"
 	"github.com/AbdulwahabNour/movies/internal/movies"
 	"github.com/gin-gonic/gin"
 )
 
-func MapMoviesRoutes(r *gin.RouterGroup, app movies.Handler) {
+func MapMoviesRoutes(r *gin.RouterGroup, app movies.Handler, mw *middlewares.MiddleWares) {
 
 	r.GET("/healthcheck", app.HealthCheckHandler)
+
 	r.GET("/movies", app.ListMoviesHandler)
-	r.POST("/movies", app.CreateMovieHandler)
 	r.GET("/movies/:id", app.ShowMovieHandler)
+
+	r.POST("/movies", app.CreateMovieHandler)
 	r.PUT("/movies/:id", app.UpdateMovieHandler)
 	r.DELETE("/movies/:id", app.DeleteMovieHandler)
 
