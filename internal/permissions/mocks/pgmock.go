8fdd38a154325f7cbbb7c4096f6a1a055c96e885
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-08-28 19:26:23 +0300

    Added mock implementations for permissions repository and service

---
+++ internal/permissions/mocks/pgmock.go
@@ -0,0 +1,46 @@
+package mocks
+
+import (
+	"context"
+
+	model "github.com/AbdulwahabNour/movies/internal/model/permission"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (m *MockRepository) AddPermission(ctx context.Context, p *model.Permission) error {
+	args := m.Called(ctx, p)
+	return args.Error(0)
+}
+
+func (m *MockRepository) GetPermission(ctx context.Context, id int64) (*model.Permission, error) {
+	args := m.Called(ctx, id)
+
+	return args.Get(0).(*model.Permission), args.Error(1)
+}
+func (m *MockRepository) UpdatePermission(ctx context.Context, p *model.Permission) error {
+	args := m.Called(ctx, p)
+	return args.Error(0)
+}
+
+func (m *MockRepository) DeletePermission(ctx context.Context, id int64) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+func (m *MockRepository) UserPermissions(ctx context.Context, userId int64) ([]*model.Permission, error) {
+	args := m.Called(ctx, userId)
+	return args.Get(0).([]*model.Permission), args.Error(1)
+}
+
+func (m *MockRepository) AddUserPermission(ctx context.Context, userpermission *model.UserPermission) error {
+	args := m.Called(ctx, userpermission)
+	return args.Error(0)
+}
+
+func (m *MockRepository) DeleteUserPermission(ctx context.Context, userpermission *model.UserPermission) error {
+	args := m.Called(ctx, userpermission)
+	return args.Error(0)
+}
---
+++ internal/permissions/mocks/servicemock.go
@@ -0,0 +1,48 @@
+package mocks
+
+import (
+	"context"
+
+	model "github.com/AbdulwahabNour/movies/internal/model/permission"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockService struct {
+	mock.Mock
+}
+
+func (m *MockService) AddPermission(ctx context.Context, p *model.Permission) error {
+	args := m.Called(ctx, p)
+	return args.Error(0)
+}
+
+func (m *MockService) GetPermission(ctx context.Context, id int64) (*model.Permission, error) {
+	args := m.Called(ctx, id)
+
+	return args.Get(0).(*model.Permission), args.Error(1)
+}
+
+func (m *MockService) UpdatePermission(ctx context.Context, p *model.Permission) error {
+	args := m.Called(ctx, p)
+	return args.Error(0)
+}
+
+func (m *MockService) DeletePermission(ctx context.Context, id int64) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (m *MockService) GetUserPermissions(ctx context.Context, userId int64) ([]*model.Permission, error) {
+	args := m.Called(ctx, userId)
+	return args.Get(0).([]*model.Permission), args.Error(1)
+}
+
+func (m *MockService) SetUserPermission(ctx context.Context, userpermission *model.UserPermission) error {
+	args := m.Called(ctx, userpermission)
+	return args.Error(0)
+}
+
+func (m *MockService) DeleteUserPermission(ctx context.Context, userpermission *model.UserPermission) error {
+	args := m.Called(ctx, userpermission)
+	return args.Error(0)
+}
