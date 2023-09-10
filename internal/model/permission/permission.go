3372845a79626c7efb63e05191313238fb3f09bb
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-09-10 20:09:41 +0300

    Add utility function for checking permission by code

--- internal/model/permission/permission.go
+++ internal/model/permission/permission.go
@@ -8,3 +8,14 @@ type UserPermission struct {
 	UserId       int64 `json:"user_id" validate:"required"`
 	PermissionId int64 `json:"permission_id" validate:"required"`
 }
+
+func HasCode(p []*Permission, code string) bool {
+
+	for _, v := range p {
+		if v.Code == code {
+			return true
+		}
+	}
+
+	return false
+}
